ba221950599f2d5fd55e0e85cbd637d8e9e40196
Deng <Landau11th@users.noreply.github.com>
2018-03-29 16:58:07 +0800

    Add files via upload

---
+++ canonical.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 28 15:26:59 2018
+
+@author: e0008730
+"""
+
+import numpy as np
+
+def canonical_energy(beta:float, eigen_energies:np.array):
+    E = eigen_energies.real
+    temp = np.exp(-beta * E)
+    Z = np.sum(temp)
+    temp = temp*E
+    return np.sum(temp)/Z
+    
+
