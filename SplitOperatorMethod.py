b34604ad73e3f7092e37bd7cde779c5c66f818f2
Landau11th <Landau11th@users.noreply.github.com>
2018-05-04 13:29:13 +0800

    a

--- SplitOperatorMethod.py
+++ SplitOperatorMethod.py
@@ -29,7 +29,7 @@ class SplitOperator:
     def one_step(self, init_state:np.array, i:int):
         #since H(t) = self.Hamiltonian(i)
         d, Eig = np.linalg.eig(self.Hamiltonian(i))
-        D = np.diag(np.exp((0-1j)*d))
+        D = np.diag(np.exp((0-1j)*d*self._dt))
         dU = np.dot(Eig, np.dot(D, Eig.conj().T))
         return np.dot(dU,init_state)
         
@@ -39,7 +39,8 @@ class SplitOperator:
         for i in range(self._N_t):
             phi = self.one_step(phi, i)
         return phi
-            
+
+#to do proper outer product and convert to 2D matrix
 def my_outer(A:np.matrix, B:np.matrix):
     temp = np.multiply.outer(A,B)    
     blk = []
@@ -52,11 +53,18 @@ def my_outer(A:np.matrix, B:np.matrix):
     
 
 class SpinChain(SplitOperator):
-    def __init__(self, num_spin : int, N_t:int, tau:float, J:float=1.):
+    def __init__(self, num_spin : int, N_t:int, tau:float, f:types.FunctionType = None, J:float=1.):
         SplitOperator.__init__(self, 2**num_spin, N_t, tau)
         self._num_spin = num_spin
         self._J = J
         self.__prepare_matrices()
+        #uniform external B field, could be time dependent
+        self._B_uniform = f
+        if self._B_uniform == None:
+            print('the external B field is not defined!')
+            print('A constant field is applied.')
+            #x here is necessary for the function to accept a variable
+            self._B_uniform = lambda x: np.array([0.5, 0.5, 0.5])
             
     def __prepare_matrices(self):
         self.identity = np.array([[1.0+0j, 0+0j],[0+0j, 1.0+0j]])
@@ -78,7 +86,7 @@ class SpinChain(SplitOperator):
             self.coupling = self.coupling - self._J*temp
         
         #uniform external field
-        self.uniform_B = np.zeros((3, self._dim, self._dim),dtype=np.complex_)
+        self.S_total = np.zeros((3, self._dim, self._dim),dtype=np.complex_)
         for i in range(3):
             for k in range(self._num_spin):
                 temp = np.matrix(1.)
@@ -87,63 +95,127 @@ class SpinChain(SplitOperator):
                         temp = my_outer(temp, self.sigma[i])
                     else:
                         temp = my_outer(temp, self.identity)
-                self.uniform_B[i] += temp
+                self.S_total[i] += temp
     
+    #could override, or simply pass a function to define the external B field
     def B(self, i:int):
-        Bx = 1.0
-        By = 1.0
-        Bz = 0.5
-        return np.array([Bx, By, Bz])
+        return self._B_uniform(i)
+    
     
     def Hamiltonian(self, i : int):
         temp = self.coupling.copy()
-        temp += np.tensordot(self.B(i), self.uniform_B, axes = 1)
+        temp += np.tensordot(self.B(i), self.S_total, axes = 1)
         return temp
         
-        
-        
+
 if __name__ == '__main__':
     
-    spins = 5
-    N_t = 2000
-    tau = 1.0
+
+    N_iters = 30
+    tau_min = 100
+    tau_max = 100000
+    N_t_min = 5000
+    dt = 0.1
+    
+    
+    beta_hot = 0.5
+    beta_cold = 1.0
+    
+    
+    taus = np.array(range(N_iters + 1))
+    taus = math.log(tau_max/tau_min)*taus/N_iters
+    taus = tau_min*np.exp(taus)
+    Ws=[]
+    count = 0
+    for tau in taus:
+        print('%d / %d  running' % (count,len(taus)),end='\r')
+        count=count+1
         
-    spinch = SpinChain(spins, N_t, tau)
+        spins = 5
+        N_t = int(tau/dt)
+        if N_t < N_t_min:
+            N_t = N_t_min
+        
+        def B_t(i:int):
+            #Bx = 0 will produce error. why?
+            #change z component
+            Bx = 0.2
+            By = 0.2
+            Bz = 0.5*np.sin((np.pi/2.0)*i/N_t)
+#            #change y component
+#            Bx = 0.1
+#            By = 0.5*np.sin((np.pi/2.0)*i/N_t)
+#            Bz = 0.2
+            return np.array([Bx, By, Bz])
+
+        spinch = SpinChain(spins, N_t, tau, B_t)
     
-#    norm = np.array([1,1,1])
-#    print(np.tensordot(norm, a.sigma, axes = 1))
+    #    norm = np.array([1,1,1])
+    #    print(np.tensordot(norm, a.sigma, axes = 1))
+        
+        H_0 = spinch.Hamiltonian(0)
+        values_0, vectors_0 = np.linalg.eig(H_0)
+        
+    #    #verify that vectors_0 are eigen column vectors
+    #    D = np.diag(values_0)
+    #    B = np.dot(np.dot(vectors_0,D), vectors_0.conj().T)
+    #    print(B-H_0)
+        init = vectors_0.copy()
+        final = spinch.time_evolution(init)
+        
+        H_tau = spinch.Hamiltonian(N_t)
+        values_tau, vectors_tau = np.linalg.eig(H_tau)
+        
+        #Amp_i->j = final_i dot vector_tau_j
+        Amp = np.dot(final.conj().T, vectors_tau)
+        Transition = np.absolute(Amp)
+        Transition = Transition*Transition
+        
+        dim = len(values_0)
+        E_0 = values_0.real
+        E_0 = np.reshape(np.repeat(E_0,dim), (dim,dim))
+        
+        E_tau = values_tau.real
+        E_tau = np.reshape(np.repeat(E_tau,dim), (dim,dim))
+        
+        #W_i->j = E_tau_j - E_0_i
+        W = E_tau.T - E_0
+        
+        beta = 1.0
+        p_0 = np.exp(-beta * values_0.real)
+        Z_0 = np.sum(p_0)
+        F_0 = -beta*math.log(Z_0)
+        p_0 = p_0/Z_0
+        
+        
+        p_tau = np.exp(-beta * values_tau.real)
+        Z_tau = np.sum(p_tau)
+        F_tau = -beta*math.log(Z_tau)
+        p_tau = p_tau/Z_tau
+        
+        #to_measure = np.exp(-beta*W.T)
+        to_measure = W.T
+        
+        Ws.append(np.dot(to_measure*Transition, p_0).sum())
+        error = np.dot(np.exp(-beta*W.T)*Transition, p_0).sum()-math.exp(-beta*(F_tau - F_0))
+        if error > 1e-10:
+            print('error=',error,'>1e-10')
     
-    H_0 = spinch.Hamiltonian(0)
-    values_0, vectors_0 = np.linalg.eig(H_0)
+    a = pd.DataFrame({'taus': taus, 'mean_W': Ws})
+    a.to_csv('average_W_z.csv')
     
-#    D = np.diag(values_0)
-#    B = np.dot(np.dot(vectors_0,D), vectors_0.conj().T)
-#    print(B-H_0)
+    fig, ax = plt.subplots()
     
+    ax.semilogx(taus, Ws, 'o')
+    #ax.semilogx(tau, sigma, 'x')
+    ax.grid()
+    #plt.savefig('test.png')
+    plt.show()
     
-#    print(values_0[0])
-#    
-#    phi = vectors_0[:,0].copy()
-#    print(np.dot(phi.conj(),phi))
-#    
-#    Hphi = np.dot(H,phi)
-#    print(np.dot(Hphi.conj(),phi))
-#
-#    phi_tau = spinch.time_evolution(phi)
-#    print(np.dot(phi_tau.conj(),phi_tau))
-#    
-#    print(np.dot(phi.conj(),phi_tau))
-    
-    init = vectors_0.copy()
-    final = spinch.time_evolution(init)
-    
-    H_tau = spinch.Hamiltonian(N_t)
-    values_tau, vectors_tau = np.linalg.eig(H_tau)
-    
-    Amp = np.dot(final.conj().T, vectors_tau)
-    Transition = np.absolute(Amp)
-    Transition = Transition*Transition
-    print(Transition)
+    #print(Transition)
+    #print(values_0)
+#    print(canonical.canonical_energy(10.0, values_0))
+#    print(canonical.canonical_energy(0.5, values_0))
 
     
         
